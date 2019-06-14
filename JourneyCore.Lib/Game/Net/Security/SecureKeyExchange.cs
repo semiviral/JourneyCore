3dbc64471beba50e7ebf719a37ad8fadb0b38780
Zavier Divelbiss <semiviral@gmail.com>
2019-06-13 17:47:20 -0700

    basic DiffieHellman

---
+++ JourneyCore.Lib/Game/Net/Security/SecureKeyExchange.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JourneyCore.Lib.Game.Net.Security
+{
+    public class SecureKeyExchange
+    {
+        private Aes Aes { get; }
+        private ECDiffieHellmanCng DiffieHellman { get; }
+
+        public byte[] PublicKey { get; }
+        public byte[] IV => Aes.IV;
+
+        public SecureKeyExchange()
+        {
+            Aes = new AesCryptoServiceProvider();
+            DiffieHellman = new ECDiffieHellmanCng()
+            {
+                KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash,
+                HashAlgorithm = CngAlgorithm.Sha256
+            };
+
+            PublicKey = DiffieHellman.PublicKey.ToByteArray();
+        }
+
+        public byte[] Encrypt(byte[] publicKey, string secretMessage)
+        {
+            byte[] encryptedMessage;
+            CngKey key = CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob);
+            byte[] derivedKey = DiffieHellman.DeriveKeyMaterial(key);
+
+            Aes.Key = derivedKey;
+
+            using (var cipherText = new MemoryStream())
+            {
+
+            }
+        }
+    }
+}
